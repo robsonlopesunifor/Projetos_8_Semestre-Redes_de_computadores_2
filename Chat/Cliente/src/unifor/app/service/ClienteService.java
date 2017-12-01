b026423e3887a6e8dcd2dbf2d5c35a4f894f0f40
Robson Lopes <robson.lopes@edu.unifor.br>
2017-12-01 18:23:23 -0200

    Chat

---
+++ Chat/Cliente/src/unifor/app/service/ClienteService.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unifor.app.service;
+
+import unifor.app.bean.ChatMessage;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class ClienteService {
+    
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect() {
+        try {
+            this.socket = new Socket("localhost", 5555);
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return socket;
+    }
+    
+    public void send(ChatMessage message) {
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
